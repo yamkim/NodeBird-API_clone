08828515f2805ebf5e844df146fbf802d20af688
yamkim <coramdeo0708@gmail.com>
2021-08-01 11:15:27 +0900

    refactor: v1RouteHandler 함수를 생성하여 동작 명시적으로 수정

--- routes/v1.js
+++ routes/v1.js
@@ -6,45 +6,60 @@ const { Domain, User } = require('../models');
 
 const router = express.Router();
 
-router.post('/token', async (req, res) => {
-  const { clientSecret } = req.body;
-  try {
-    const domain = await Domain.findOne({
-      where: { clientSecret },
-      include: {
-        model: User,
-        attribute: ['nick', 'id'],
-      },
+async function readDomainRecord(req, res) {
+    const { clientSecret } = req.body;
+    const ret = await Domain.findOne({
+        where: { clientSecret },
+        include: {
+            model: User,
+            attributes: ['nick', 'id'],
+        }
     });
-    if (!domain) {
-      return res.status(401).json({
-        code: 401,
-        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
-      });
+    if (!ret) {
+        return res.status(401).json({
+            code: 401,
+            message: '등록되지 않은 도메인입니다.'
+        });
     }
-    const token = jwt.sign({
-      id: domain.User.id,
-      nick: domain.User.nick,
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1m', // 1분
-      issuer: 'nodebird',
-    });
-    return res.json({
-      code: 200,
-      message: '토큰이 발급되었습니다',
-      token,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: '서버 에러',
-    });
-  }
-});
+    return ret;
+}
 
-router.get('/test', verifyToken, (req, res) => {
-  res.json(req.decoded);
-});
+async function signJwtToken(domain) {
+    const ret = await jwt.sign({
+            id: domain.User.id,
+            nick: domain.User.nick,
+        }, process.env.JWT_SECRET, {
+            expiresIn: '10m',
+            issuer: 'nodebird',
+        });
+    return ret;
+}
+
+const v1RouteHandler = {
+    setJwtToken: async function(req, res, next) {
+        try {
+            const domain = await readDomainRecord(req, res);
+            const token = await signJwtToken(domain);
+            return res.json({
+                code: 200,
+                message: '토큰이 발급되었습니다.',
+                token,
+            });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        }
+    },
+    testJwtToken: function(req, res, next) {
+        res.json(req.decoded);
+    }
+
+}
+
+router.post('/token', v1RouteHandler.setJwtToken);
+router.get('/test', verifyToken, v1RouteHandler.testJwtToken);
 
 module.exports = router;
