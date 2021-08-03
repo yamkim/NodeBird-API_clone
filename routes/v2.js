673df5efe55489e197847d9808a2bfcc6e1ac9a2
yamkim <coramdeo0708@gmail.com>
2021-08-03 13:53:47 +0900

    feat: v2.js cors 에러 해결부분 추가

--- routes/v2.js
+++ routes/v2.js
@@ -1,9 +1,13 @@
+const url = require('url');
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const { verifyToken, apiLimiter } = require('./middlewares');
 const { Post, Domain, User, Hashtag } = require('../models');
-const { handleResponse } = require('../libs/handleResponse')
+const { handleResponse } = require('../libs/handleResponse');
+const { urlencoded } = require('express');
+const { compareSync } = require('bcrypt');
 
 const router = express.Router();
 
@@ -78,12 +82,34 @@ const v2RouteHandler = {
         }
     },
 
-    testJwtToken: function(req, res, next) {
+    checkValidOrigin: async function (req, res, next) {
+        const domain = await Domain.findOne({
+            // request header의 origin을 파싱하여 domain DB에 있는지 찾아봅니다.
+            // NOTE: https://www.opentutorials.org/module/938/7369
+            where: {
+                host: url.parse(req.get('origin')).host,
+            },
+        });
+        // NOTE: (req, res, next)를 추가하는 부분에 대한 학습 필요!
+        if (domain) {
+            cors({
+                origin: req.get('origin'), // 등록되어있는 domain에 대해서 출처를 미리 명시합니다.
+                credentials: true,
+            })(req, res, next);
+        } else {
+            next();
+        }
+    }, 
+
+    testJwtToken: function(req, res) {
         res.json(req.decoded);
     }
 
 }
 
+// router.use(cors({ credentials: true })); // 모든 주소에 대해 api 요청을 허용하는 경우입니다.
+//                                          // Access-Control-Allow-Origin이 *로 설정됩니다.
+router.use(v2RouteHandler.checkValidOrigin);
 router.post('/token', apiLimiter, v2RouteHandler.setJwtToken);
 router.get('/test', verifyToken, apiLimiter, v2RouteHandler.testJwtToken);
 router.get('/posts/my', verifyToken, apiLimiter, v2RouteHandler.returnAllPostsForUser);
